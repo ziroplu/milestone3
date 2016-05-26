7feeed548f1aaa4f0f3d8bf5026b9ade13325adb
Ziroli Plutschow <ziroli.plutschow@ergon.ch>
2016-05-26 21:22:53 +0200

    rc-1

--- script.js
+++ script.js
@@ -3,44 +3,38 @@ jQuery.noConflict();
 (function( $ ) {
   $(function() {
 
-    // var state = localStorage.getItem('issueTacker');
-
-    // if(state != null){
-    // 	    $('body').empty();
-    // 		$('body').append(state);
-    // 		$('.todoItem.done').find('input:checkbox').attr('checked', true);
-    // }
-
     //$('.datepicker').datepicker('option', {dateFormat: 'yy-mm-dd' });
     $('#datepicker').datepicker({dateFormat: 'yy-mm-dd' });
-  	var formArray = [];
 
-    loadIssue(214);
+    bindSaveProject();
+    bindLoadProject();
+
+    var project =  JSON.parse(localStorage.getItem("project"));
+    if (project != null) {
+      document.getElementById("eingabefeld").value=project.title;
+      loadIssue(project.id);
+    }
 
   	$('#createIssue').on('submit', function(e){
-      formArray = $(this).serializeArray();
 
   		var issue = {done:false};
-      var noOfIssues = localStorage.getItem("noOfIssues");
-      issue.id = noOfIssues++;
-      localStorage.setItem("noOfIssues", noOfIssues);
 
-      issue.project_id = 214;
+      var project =  JSON.parse(localStorage.getItem("project"));
+      issue.project_id = project.id;
       issue.client_id = guid();
       issue.done = false;
 
-      // ugly
-      issue.title = String(formArray[1]['value']);
-      issue.priority = "2"; // TODO
-      issue.due_date = String(formArray[0]['value']);
+      issue.title = document.getElementById("issueText").value;
+      issue.priority = document.getElementById("issueSelect").value;
+      issue.due_date = document.getElementById("datepicker").value;
 
-      console.log("1 createIssue");
+      console.log("createIssue " + issue.id);
       console.log(issue);
       console.log(JSON.stringify(issue));
       postCreate(issue);
 
-
   		e.preventDefault();
+      this.reset();
   	});
 
   function loadIssue (projectId) {
@@ -52,23 +46,70 @@ jQuery.noConflict();
        contentType: 'application/json',
        async: true,
        success: function (response) {
-         localStorage.setItem("noOfIssues", response.length);
+//         document.getElementById("todolist").children[0].children[0].children[0].remove();
+        $("#issueTable tr").remove();
+
          for (var i in response) {
            var issue = response[i];
            printIssue(issue);
-           localStorage.setItem("n"+issue.id, JSON.stringify(issue));
+           localStorage.setItem(issue.client_id, JSON.stringify(issue));
          }
-         console.log('issues created. n=' + response.length);
+         console.log('issues loaded. n=' + response.length);
        }
    });
+  }
+
+  function createProject(project) {
+    $.ajax
+   ({
+       type: "POST",
+       url: 'http://zhaw-weng-api.herokuapp.com/api/projects/',
+       dataType: 'json',
+       contentType: 'application/json',
+       async: false,
+       //json object to sent to the authentication url
+       data: JSON.stringify(project),
+       error: function () {
+         console.log('.');
+       },
+       success: function (responseIssue) {
+         project = responseIssue;
+         console.log('project created. ' + JSON.stringify(project));
+         localStorage.setItem("project", JSON.stringify(project));
+         document.getElementById("projectId").innerHTML = "id:" + project.id;
+       }
+   })
+  }
+
+  function getProject(id) {
+    $.ajax
+   ({
+       type: "GET",
+       url: 'http://zhaw-weng-api.herokuapp.com/api/projects/' + id,
+       dataType: 'json',
+       contentType: 'application/json',
+       async: false,
+       //json object to sent to the authentication url
+       data: JSON.stringify(project),
+       error: function () {
+         console.log('project not available ' + id);
+       },
+       success: function (responseIssue) {
+         project = responseIssue;
+         console.log('project loaded. ' + JSON.stringify(project));
+         localStorage.setItem("project", JSON.stringify(project));
+         document.getElementById("projectId").innerHTML = "id:" + project.id;
+         document.getElementById("eingabefeld").innerHTML = project.title;
+         loadIssue(id);
+       }
+   })
   }
 
   function postCreate(issue) {
     $.ajax
    ({
        type: "POST",
-       //url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues',
-       url: 'http://zhaw-weng-api.herokuapp.com/api/project/214/issues',
+       url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues',
        dataType: 'json',
        contentType: 'application/json',
        async: false,
@@ -77,7 +118,7 @@ jQuery.noConflict();
        success: function (responseIssue) {
          console.log( 'issue created. original id='+ issue.id);
          console.log(responseIssue);
-         localStorage.setItem("n"+responseIssue.id, JSON.stringify(responseIssue));
+         localStorage.setItem(responseIssue.client_id, JSON.stringify(responseIssue));
          printIssue(responseIssue);
        }
    })
@@ -87,8 +128,7 @@ jQuery.noConflict();
     $.ajax
    ({
        type: "PUT",
-       //url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues',
-       url: 'http://zhaw-weng-api.herokuapp.com/api/project/214/issues/' + issue.id,
+       url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues/' + issue.id,
        dataType: 'json',
        contentType: 'application/json',
        async: false,
@@ -104,8 +144,7 @@ jQuery.noConflict();
     $.ajax
    ({
        type: "DELETE",
-       //url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues',
-       url: 'http://zhaw-weng-api.herokuapp.com/api/project/214/issues/' + issue.id,
+       url: 'http://zhaw-weng-api.herokuapp.com/api/project/' + issue.project_id + '/issues/' + issue.id,
        dataType: 'json',
        contentType: 'application/json',
        async: false,
@@ -119,54 +158,74 @@ jQuery.noConflict();
 
 function printIssue (issue) {
     console.log("print " + issue.title);
-    var issueRow = '<tr class="todoItem"> <td><input type="checkbox" name="bla" id=' + issue.id + ' /></td> <td>' + issue.title + '</td> <td>' + issue.due_date + '</td> <td><img id="bin" class="delete" src="bin.png"></td> </tr>';
+    var issueRow = '<tr id="' + issue.client_id + '" class="todoItem '+ (issue.done ? "done" : "" ) + ' "> <td><input id="' + issue.client_id + '" type="checkbox" name="bla"/></td> <td>' + issue.title + '</td> <td>' + issue.due_date + '</td> <td><img id="bin" class="delete" src="bin.png"></td> </tr>';
     $('.tabelle tbody').prepend(issueRow);
-    if (!issue.done) {
-      $( ".todoItem input:checkbox" ).unbind();
-    }
+
+    $( ".todoItem input:checkbox" ).unbind();
     $( ".todoItem .delete" ).unbind();
 
     bindDone();
     bindDelete();
 
-    $(this).trigger("reset");
 }
 
   	function bindDone(){
 	  	$( ".todoItem input:checkbox" ).on('click',
-        function(){
-    			$(this).parents('.todoItem').toggleClass('done');
+        function () {
+          $(this).parents('.todoItem').toggleClass('done');
 
-          var issue = JSON.parse(localStorage.getItem("n"+this.id));
+          var issue = JSON.parse(localStorage.getItem(this.id));
           issue.done = !issue.done;
 
           // update localStorage
-          localStorage.setItem("n"+issue.id, JSON.stringify(issue));
+          localStorage.setItem(issue.client_id, JSON.stringify(issue));
           // call REST to update
           putUpdate(issue);
 
-          console.log("2-bind Done");
+          console.log("2-Done");
     		});
   	}
 
-
   	function bindDelete(){
-		$( ".todoItem .delete" ).on('click', function(){
+		$( ".todoItem .delete" ).on('click', function () {
       // get id
-      var id = this.parentNode.parentNode.getElementsByTagName("input")[0].id;
+      //var id = this.parentNode.parentNode.getElementsByTagName("input")[0].id;
 
+      var id = this.parentNode.parentElement.id;
       // get issue
-      var issue = JSON.parse(localStorage.getItem("n"+id));
+      var issue = JSON.parse(localStorage.getItem(id));
+
+      // remove from page
+      // $(this).parents('.todoItem').remove();
+      this.parentNode.parentElement.remove();
+
+      // remove in local Storage
+      localStorage.removeItem(id);
+
       // call REST to delete
       deleteRest(issue);
-      // remove from page
-      $(this).parents('.todoItem').remove();
-      // remove in local Storage
-      localStorage.removeItem("n"+id);
 
-      console.log("3- bind Delete");
+      console.log("Delete Item " + issue.title);
 		});
   	}
+
+    function bindSaveProject () {
+      $("#buttonProjectNameSave").on('click',
+      function () {
+        var project = {id:0};
+        project.title = document.getElementById("eingabefeld").value;
+        createProject(project);
+      });
+    }
+
+    function bindLoadProject () {
+      $("#buttonProjectNameLoad").on('click',
+      function () {
+        var projectId = document.getElementById("eingabefeld").value;
+        getProject(projectId);
+        document.getElementById("eingabefeld").value=JSON.parse(localStorage.getItem("project")).title;
+      });
+    }
 
   });
 
